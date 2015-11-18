0a9f20ef5a195012d32bf59e5edc112a2304e97a
Sebastian Lau <lauseb644@gmail.com>
2015-11-18 14:14:35 +0100

    Add config conference-specific variables

---
+++ config.py
@@ -0,0 +1,12 @@
+
+
+conference_name = r'43,5. Konferenz der deutschsprachigen Informatik-Fachschaften'
+conference_info = r"""\footnote{Wikipedia: Konferenz der Informatikfachschaften (\url{https://de.wikipedia.org/wiki/Konferenz_der_Informatikfachschaften}) und Was ist die KIF? (\url{https://kif.fsinf.de/wiki/Was_ist_die_KIF}). Diesen Brief finden Sie auch unter \resourl.}""".encode()
+fromaddress = r"""c/o Fachschaft Informatik am Institut für Informatik der Unversität Bonn\\Römerstraße 164\\53117 Bonn""".encode()
+backaddress = """{0}, {1}""".format(conference_name, fromaddress).encode()
+date = "15. November 2014"
+place = "Bonn"
+signature = "die Teilnehmer und Teilnehmerinnen der {0}".format(conference_name)
+logoname = "465px-Kif_logo_435"
+resourl = r"""https://kif.fsinf.de/wiki/KIF435:Resolutionen/Wissenschaftszeitvertragsgesetz""".encode()
+closing = "Hochachtungsvoll"
