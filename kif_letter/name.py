f16569cd05bdac8c8b7a19be36019876ac39561f
Sebastian Lau <lauseb644@gmail.com>
2016-03-07 19:42:57 +0100

    Handle pre- & suffixes like 'Dr.', ' van' and double names in genderdetector

--- kif_letter/name.py
+++ kif_letter/name.py
@@ -10,11 +10,24 @@ from genderdetect import genderDetector
 class Name:
     def __init__(self, vorname, nachname, partei):
         self.vorname = vorname
+        self.vorname_raw = vorname
         self.nachname = nachname
         self.partei = partei
 
+        # Vornamen auf elementaren Teil reduzieren
+        dotted_prefix_end = self.vorname_raw.rfind('. ')
+        if dotted_prefix_end > -1:
+            self.vorname_raw = self.vorname_raw[dotted_prefix_end+2:]
+        double_name_splitter = self.vorname_raw.rfind('-')
+        if double_name_splitter > -1:
+            self.vorname_raw = self.vorname_raw.partition('-')[0]
+        # Strip other suffixes
+        van_sufix = self.vorname_raw.rfind(' van')
+        if van_sufix > -1:
+            self.vorname_raw = self.vorname_raw[:van_sufix]
+
         if config.genderdetector == 'GenderizeIO':
-            gender = Genderize().get(vorname)
+            gender = Genderize().get(self.vorname_raw)
             if gender[0]['gender'] == 'male':
                 self.gender = 'm'
             elif gender[0]['gender'] == 'female':
@@ -22,7 +35,7 @@ class Name:
             else:
                 self.gender = ''
         else:
-            gender = genderDetector.guess(self.vorname)
+            gender = genderDetector.guess(self.vorname_raw)
             if gender == 'male':
                 self.gender = 'm'
             elif gender == 'female':
