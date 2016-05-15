6497b78d81e59ef4b6d9b9f8ae7d770bd1af0bd4
Sebastian Lau <lauseb644@gmail.com>
2016-05-15 21:26:49 +0200

    Add toaddress to config

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ conference_name = '43,5. Konferenz der Informatik-Fachschaften'
 conference_info = r"""\footnote{Wikipedia: Konferenz der Informatikfachschaften (\url{https://de.wikipedia.org/wiki/Konferenz_der_Informatikfachschaften}) und Was ist die KIF? (\url{https://kif.fsinf.de/wiki/Was_ist_die_KIF}). Diesen Brief finden Sie auch unter \resourl.}"""
 fromaddress = r"""c/o Fachschaft Informatik am Institut für Informatik der Unversität Bonn\\Römerstraße 164\\53117 Bonn"""
 backaddress = r"""{0}, {1}""".format(conference_name, fromaddress)
+toaddress = r""""""
 date = "15. November 2014"
 place = "Bonn"
 signature = "die Teilnehmer und Teilnehmerinnen der {0}".format(conference_name)
