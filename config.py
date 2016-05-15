18e57ecc8c8420e810c903b51f2c578abad0863f
Sebastian Lau <lauseb644@gmail.com>
2016-05-15 21:48:40 +0200

    Update conference config data

--- config.py
+++ config.py
@@ -4,15 +4,15 @@
 import codecs
 
 
-conference_name = '43,5. Konferenz der Informatik-Fachschaften'
+conference_name = '44,0. Konferenz der Informatik-Fachschaften'
 conference_info = r"""\footnote{Wikipedia: Konferenz der Informatikfachschaften (\url{https://de.wikipedia.org/wiki/Konferenz_der_Informatikfachschaften}) und Was ist die KIF? (\url{https://kif.fsinf.de/wiki/Was_ist_die_KIF}). Diesen Brief finden Sie auch unter \resourl.}"""
-fromaddress = r"""c/o Fachschaft Informatik am Institut für Informatik der Unversität Bonn\\Römerstraße 164\\53117 Bonn"""
+fromaddress = r"""c/o Technische Universität Darmstadt\\Fachschaft Informatik\\Hochschulstr.\\64289 Darmstadt"""
 backaddress = r"""{0}, {1}""".format(conference_name, fromaddress)
 toaddress = r""""""
-date = "15. November 2014"
-place = "Bonn"
+date = "08. Mai 2016"
+place = "Darmstadt"
 signature = "die Teilnehmer und Teilnehmerinnen der {0}".format(conference_name)
-logoname = "Kif_logo_435.png"
+logoname = "Kif_logo_440.png"
 scalefactor = 0.05
 resourl = """https://kif.fsinf.de/wiki/KIF435:Resolutionen/Wissenschaftszeitvertragsgesetz"""
 closing = "Hochachtungsvoll"
